c3b727fdec30aab5bd244613ab64245379edc99c
LopezS7 <samirisraellopez21@gmail.com>
2024-05-28 22:28:39 -0600

    se agrega la logica de capas sobre la tabla contacto

--- src/main/java/com/TicoTaco/TicoTaco/Repository/ContactoRepository.java
+++ src/main/java/com/TicoTaco/TicoTaco/Repository/ContactoRepository.java
@@ -1,2 +1,7 @@
-package com.TicoTaco.TicoTaco.Repository;public interface ContactoRepository {
+package com.TicoTaco.TicoTaco.Repository;
+
+import com.TicoTaco.TicoTaco.model.ContactoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactoRepository extends JpaRepository<ContactoModel, Long> {
 }
--- src/main/java/com/TicoTaco/TicoTaco/service/ContactoService.java
+++ src/main/java/com/TicoTaco/TicoTaco/service/ContactoService.java
@@ -1,2 +1,49 @@
-package com.TicoTaco.TicoTaco.service;public class ContactoService {
+package com.TicoTaco.TicoTaco.service;
+
+import com.TicoTaco.TicoTaco.Repository.ContactoRepository;
+import com.TicoTaco.TicoTaco.model.ContactoModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContactoService {
+
+    private final ContactoRepository contactoRepository;
+
+    @Autowired
+    public ContactoService(ContactoRepository contactoRepository) {
+        this.contactoRepository = contactoRepository;
+    }
+
+    public List<ContactoModel> getAllContactos() {
+        return contactoRepository.findAll();
+    }
+
+    public Optional<ContactoModel> getContactoById(Long id) {
+        return contactoRepository.findById(id);
+    }
+
+    public ContactoModel createContacto(ContactoModel contacto) {
+        return contactoRepository.save(contacto);
+    }
+
+    public ContactoModel updateContacto(long id, ContactoModel contactoDetails) {
+        ContactoModel contacto = contactoRepository.findById(id).orElse(null);
+
+        if (contacto != null) {
+            contacto.setTelefono(contactoDetails.getTelefono());
+            contacto.setEmail(contactoDetails.getEmail());
+
+            return contactoRepository.save(contacto);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteContacto(Long id) {
+        contactoRepository.deleteById(id);
+    }
 }
