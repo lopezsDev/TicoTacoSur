c8a191b9a305529b89d039143c80c4033e5fb281
LopezS7 <samirisraellopez21@gmail.com>
2024-05-28 20:24:28 -0600

    Se completa la logica de la tabla AdminRestaurante

---
+++ src/main/java/com/TicoTaco/TicoTaco/Repository/AdminRestauranteRepository.java
@@ -0,0 +1,9 @@
+package com.TicoTaco.TicoTaco.Repository;
+
+import com.TicoTaco.TicoTaco.model.AdminRestauranteModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRestauranteRepository extends JpaRepository<AdminRestauranteModel, Integer> {
+}
