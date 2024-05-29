8472d8509136eae7bf67e4049001a6214c360dbd
LopezS7 <samirisraellopez21@gmail.com>
2024-05-28 22:35:03 -0600

    Se agrega la capa de logica de la tabla costos operativos

---
+++ src/main/java/com/TicoTaco/TicoTaco/Repository/CostosOperativosRepository.java
@@ -0,0 +1,7 @@
+package com.TicoTaco.TicoTaco.Repository;
+
+import com.TicoTaco.TicoTaco.model.CostosOperativosModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CostosOperativosRepository extends JpaRepository<CostosOperativosModel, Long> {
+}
