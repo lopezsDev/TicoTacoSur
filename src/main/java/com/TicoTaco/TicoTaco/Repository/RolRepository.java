fe6e64032f4c81a79a68944381b4d47c53ccd7c0
LopezS7 <samirisraellopez21@gmail.com>
2024-05-30 14:03:26 -0600

    se agrega la logica sobre la tabla rol

---
+++ src/main/java/com/TicoTaco/TicoTaco/Repository/RolRepository.java
@@ -0,0 +1,7 @@
+package com.TicoTaco.TicoTaco.Repository;
+
+import com.TicoTaco.TicoTaco.model.RolModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<RolModel, Long> {
+}
