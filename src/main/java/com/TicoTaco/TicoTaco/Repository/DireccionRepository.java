3fc313be615324c6e5c3b03ba1faeb48df1b96a5
LopezS7 <samirisraellopez21@gmail.com>
2024-05-30 10:42:34 -0600

    Se agrega la capa de logica de la tabla direccion

---
+++ src/main/java/com/TicoTaco/TicoTaco/Repository/DireccionRepository.java
@@ -0,0 +1,7 @@
+package com.TicoTaco.TicoTaco.Repository;
+
+import com.TicoTaco.TicoTaco.model.DireccionModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DireccionRepository extends JpaRepository<DireccionModel, Long> {
+}
---
+++ src/main/java/com/TicoTaco/TicoTaco/service/DireccionService.java
@@ -0,0 +1,49 @@
+package com.TicoTaco.TicoTaco.service;
+
+import com.TicoTaco.TicoTaco.Repository.DireccionRepository;
+import com.TicoTaco.TicoTaco.model.DireccionModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DireccionService {
+
+    private final DireccionRepository direccionRepository;
+
+    @Autowired
+    public DireccionService(DireccionRepository direccionRepository) {
+        this.direccionRepository = direccionRepository;
+    }
+
+    public List<DireccionModel> getAllDirecciones() {
+        return direccionRepository.findAll();
+    }
+
+    public Optional<DireccionModel> getDireccionById(long id) {
+        return direccionRepository.findById(id);
+    }
+
+    public DireccionModel createDireccion(DireccionModel direccion) {
+        return direccionRepository.save(direccion);
+    }
+
+    public DireccionModel updateDireccion(long id, DireccionModel direccionDetails) {
+        DireccionModel direccion = direccionRepository.findById(id).orElse(null);
+
+        if (direccion != null) {
+            direccion.setDireccionEspecifica(direccionDetails.getDireccionEspecifica());
+            direccion.setDistrito(direccionDetails.getDistrito());
+
+            return direccionRepository.save(direccion);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteDireccion(Long id) {
+        direccionRepository.deleteById(id);
+    }
+}
