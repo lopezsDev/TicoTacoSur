df20857426765fc13b433d31c4cbef5b066ca2c8
LopezS7 <samirisraellopez21@gmail.com>
2024-05-30 15:43:20 -0600

    se agrega la logica sobre la tabla tipo costos operativos

---
+++ src/main/java/com/TicoTaco/TicoTaco/Repository/TipoCostosOperativosRepository.java
@@ -0,0 +1,7 @@
+package com.TicoTaco.TicoTaco.Repository;
+
+import com.TicoTaco.TicoTaco.model.TipoCostosOperativosModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoCostosOperativosRepository extends JpaRepository<TipoCostosOperativosModel, Long> {
+}
---
+++ src/main/java/com/TicoTaco/TicoTaco/service/TipoCostosOperativosService.java
@@ -0,0 +1,47 @@
+package com.TicoTaco.TicoTaco.service;
+
+import com.TicoTaco.TicoTaco.Repository.TipoCostosOperativosRepository;
+import com.TicoTaco.TicoTaco.model.TipoCostosOperativosModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TipoCostosOperativosService {
+
+    private final TipoCostosOperativosRepository tipoCostosOperativosRepository;
+
+    @Autowired
+    public TipoCostosOperativosService(TipoCostosOperativosRepository tipoCostosOperativosRepository) {
+        this.tipoCostosOperativosRepository = tipoCostosOperativosRepository;
+    }
+
+    public List<TipoCostosOperativosModel> getAllTiposCostosOperativos() {
+        return tipoCostosOperativosRepository.findAll();
+    }
+
+    public Optional<TipoCostosOperativosModel> getTipoCostosOperativosById(long id) {
+        return tipoCostosOperativosRepository.findById(id);
+    }
+
+    public TipoCostosOperativosModel createTipoCostosOperativos(TipoCostosOperativosModel tipoCostosOperativos) {
+        return tipoCostosOperativosRepository.save(tipoCostosOperativos);
+    }
+
+    public TipoCostosOperativosModel updateTipoCostosOperativos(long id, TipoCostosOperativosModel tipoCostosOperativosDetails) {
+        TipoCostosOperativosModel tipoCostosOperativos = tipoCostosOperativosRepository.findById(id).orElse(null);
+
+        if (tipoCostosOperativos != null) {
+            tipoCostosOperativos.setNombreTipoCostosOperativos(tipoCostosOperativosDetails.getNombreTipoCostosOperativos());
+            return tipoCostosOperativosRepository.save(tipoCostosOperativos);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteTipoCostosOperativos(long id) {
+        tipoCostosOperativosRepository.deleteById(id);
+    }
+}
