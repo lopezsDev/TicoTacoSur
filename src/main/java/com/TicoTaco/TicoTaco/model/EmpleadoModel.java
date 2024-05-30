3c476edb915a1db7a728f8a7998650a6139ea62b
LopezS7 <samirisraellopez21@gmail.com>
2024-05-30 11:10:35 -0600

    Se agrega la capa de logica de la tabla empleado

---
+++ src/main/java/com/TicoTaco/TicoTaco/model/EmpleadoModel.java
@@ -0,0 +1,39 @@
+package com.TicoTaco.TicoTaco.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "EMPLEADO")
+public class EmpleadoModel {
+
+    @Id
+    @Column(name = "C_EMPLEADO")
+    private Long empleadoId;
+
+    @Column(name = "D_NOMBRE_EMPLEADO")
+    private String nombreEmpleado;
+
+    @Column(name = "D_APELLIDO_EMPLEADO")
+    private String apellidoEmpleado;
+
+    @Column(name = "F_CONTRATACION")
+    private java.util.Date fechaContratacion;
+
+    @Column(name = "C_PUESTO")
+    private int puestoId;
+
+    @Column(name = "C_SUCURSAL")
+    private int sucursalId;
+
+    @Column(name = "C_PLANILLA")
+    private int planillaId;
+
+    @Column(name = "C_PERMISO")
+    private int permisoId;
+}
+
