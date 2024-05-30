f0d199bf3e5e407a1ab2dbd4017f8cbcd6c1692e
LopezS7 <samirisraellopez21@gmail.com>
2024-05-30 13:52:35 -0600

    se agrega la logica sobre la tabla proveedor

--- src/main/java/com/TicoTaco/TicoTaco/model/ProveedorModel.java
+++ src/main/java/com/TicoTaco/TicoTaco/model/ProveedorModel.java
@@ -1,2 +1,24 @@
-package com.TicoTaco.TicoTaco.model;public class ProveedorModel {
+package com.TicoTaco.TicoTaco.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "PROVEEDOR")
+public class ProveedorModel {
+
+    @Id
+    @Column(name = "C_PROVEEDOR")
+    private long proveedorId;
+
+    @Column(name = "D_NOMBRE_PROVEEDOR")
+    private String nombreProveedor;
+
+    @Column(name = "C_CONTACTO")
+    private long contactoId;
 }
+
--- src/main/java/com/TicoTaco/TicoTaco/service/ProveedorService.java
+++ src/main/java/com/TicoTaco/TicoTaco/service/ProveedorService.java
@@ -1,2 +1,49 @@
-package com.TicoTaco.TicoTaco.service;public class ProveedorService {
+package com.TicoTaco.TicoTaco.service;
+
+import com.TicoTaco.TicoTaco.Repository.ProveedorRepository;
+import com.TicoTaco.TicoTaco.model.ProveedorModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProveedorService {
+
+    private final ProveedorRepository proveedorRepository;
+
+    @Autowired
+    public ProveedorService(ProveedorRepository proveedorRepository) {
+        this.proveedorRepository = proveedorRepository;
+    }
+
+    public List<ProveedorModel> getAllProveedores() {
+        return proveedorRepository.findAll();
+    }
+
+    public Optional<ProveedorModel> getProveedorById(long id) {
+        return proveedorRepository.findById(id);
+    }
+
+    public ProveedorModel createProveedor(ProveedorModel proveedor) {
+        return proveedorRepository.save(proveedor);
+    }
+
+    public ProveedorModel updateProveedor(long id, ProveedorModel proveedorDetails) {
+        ProveedorModel proveedor = proveedorRepository.findById(id).orElse(null);
+
+        if (proveedor != null) {
+            proveedor.setNombreProveedor(proveedorDetails.getNombreProveedor());
+            proveedor.setContactoId(proveedorDetails.getContactoId());
+
+            return proveedorRepository.save(proveedor);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteProveedor(long id) {
+        proveedorRepository.deleteById(id);
+    }
 }
