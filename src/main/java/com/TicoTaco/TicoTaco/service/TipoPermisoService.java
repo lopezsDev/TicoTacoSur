374444e5ce578888d8f7f4882b08da92d1ad918b
LopezS7 <samirisraellopez21@gmail.com>
2024-06-04 14:23:24 -0600

    Se corrije el service del tipo permiso

--- src/main/java/com/TicoTaco/TicoTaco/service/TipoPermisoService.java
+++ src/main/java/com/TicoTaco/TicoTaco/service/TipoPermisoService.java
@@ -3,10 +3,12 @@ package com.TicoTaco.TicoTaco.service;
 import com.TicoTaco.TicoTaco.Repository.TipoPermisoRepository;
 import com.TicoTaco.TicoTaco.model.TipoPermisoModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class TipoPermisoService {
     private final TipoPermisoRepository tipoPermisoRepository;
 
@@ -15,33 +17,30 @@ public class TipoPermisoService {
         this.tipoPermisoRepository = tipoPermisoRepository;
     }
 
-    public List<TipoPermisoModel>getAllTipoPermiso(){
+    public List<TipoPermisoModel> getAllTipoPermiso() {
         return tipoPermisoRepository.findAll();
     }
 
-    public Optional<TipoPermisoModel> getTipoPermisoById(long id){return tipoPermisoRepository.findById(id);
+    public Optional<TipoPermisoModel> getTipoPermisoById(long id) {
+        return tipoPermisoRepository.findById(id);
     }
 
-    public TipoPermisoModel createTipoPermiso(TipoPermisoModel tipoPermiso){
+    public TipoPermisoModel createTipoPermiso(TipoPermisoModel tipoPermiso) {
         return tipoPermisoRepository.save(tipoPermiso);
     }
 
-    public TipoPermisoRepository updateTipoPermisoRepository(long id, TipoPermisoModel tipoPermisoDetails) {
-        TipoPermisoModel tipopermiso = tipoPermisoRepository.findById(id).orElse(null);
-        if (tipopermiso != null) {
-            tipopermiso.setTipoPermisoId(tipoPermisoDetails.getTipoPermisoId());
-            tipopermiso.setNombrePermiso(tipoPermisoDetails.getNombrePermiso());
-            return (TipoPermisoRepository) tipoPermisoRepository.save(tipopermiso);
-        }else {
-
+    public TipoPermisoModel updateTipoPermiso(long id, TipoPermisoModel tipoPermisoDetails) {
+        TipoPermisoModel tipoPermiso = tipoPermisoRepository.findById(id).orElse(null);
+        if (tipoPermiso != null) {
+            tipoPermiso.setTipoPermisoId(tipoPermisoDetails.getTipoPermisoId());
+            tipoPermiso.setNombrePermiso(tipoPermisoDetails.getNombrePermiso());
+            return tipoPermisoRepository.save(tipoPermiso);
+        } else {
             return null;
         }
-
-
     }
 
     public void deleteTipoPermiso(long id) {
         tipoPermisoRepository.deleteById(id);
     }
 }
-
